e4485f030136a9bc25e549fce460a4f62e19e62b
Petr Hučík <petr@getdatakick.com>
2024-12-17 08:37:13 +0100

    Releaes statsmodule v2.7.0

--- statsmodule.php
+++ statsmodule.php
@@ -87,7 +87,7 @@ class StatsModule extends ModuleStats
     {
         $this->name = 'statsmodule';
         $this->tab = 'analytics_stats';
-        $this->version = '2.6.0';
+        $this->version = '2.7.0';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
